6264efff4385596f1072d099ae521000bc0e4dc4
Somhairle H. Marisol <muqiu-han@outlook.com>
2025-07-28 17:57:37 +0800

    refactor: Modify the version update logic of package.json in the project root directory in the changeset-version script

---
+++ .changeset/seven-rules-fly.md
@@ -0,0 +1,9 @@
+---
+"@app/api": patch
+"@app/web": patch
+"@repo/bar": patch
+"@repo/database": patch
+"@repo/foo": patch
+---
+
+Modify the version update logic of package.json in the project root directory in the changeset-version script
--- scripts/changeset-version.sh
+++ scripts/changeset-version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
+git cliff --unreleased --bump --context | jq -r ".[0].version" | xargs -I {} npm version {} --git-tag-version=false
 git cliff --unreleased --output NEXT-CHANGELOG-ENTRY.md --strip header --bump
-git cliff --unreleased --bump --context | jq -r ".[0].version" | xargs npm version
 npx changeset version
