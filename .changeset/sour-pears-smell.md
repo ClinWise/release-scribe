94a0889176ec784d9c449d57d48dfec97b0610d2
Somhairle H. Marisol <muqiu-han@outlook.com>
2025-07-28 18:55:09 +0800

    fix: Attempt to fix the issue where the modification to NEXT-CHANGELOG-ENTRY.md in the version package pr is invalid

---
+++ .changeset/sour-pears-smell.md
@@ -0,0 +1,9 @@
+---
+"@app/api": patch
+"@app/web": patch
+"@repo/bar": patch
+"@repo/database": patch
+"@repo/foo": patch
+---
+
+Attempt to fix the issue where the modification to NEXT-CHANGELOG-ENTRY.md in the version package pr is invalid
--- scripts/changeset-version.sh
+++ scripts/changeset-version.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 git cliff --unreleased --bump --context | jq -r ".[0].version" | xargs -I {} npm version {} --git-tag-version=false
-git cliff --unreleased --output NEXT-CHANGELOG-ENTRY.md --strip header --bump
+
+if [ ! -s NEXT-CHANGELOG-ENTRY.md ]; then  
+  git cliff --unreleased --output NEXT-CHANGELOG-ENTRY.md --strip header --bump
+fi
+
 npx changeset version
